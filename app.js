b1f37edeedba2237f1f3a25de0ea15416623fa6b
Tanjum kadakol <84380051+Tanjum786@users.noreply.github.com>
2021-11-26 16:48:36 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var buttentranslator=document.querySelector("butn-translate");
-temp1
+// var username=prompt("enter name");
+// alert("your name is"+username);
+var buttentranslator=document.querySelector("#butn-translate");
+var textinput=document.querySelector("#text-input");
+console.log(textinput);
+
+ function clickEventHandler(){
+     console.log("clicked");
+     console.log("input",textinput.value);
+};
+buttentranslator.addEventListener("click",clickEventHandler);
